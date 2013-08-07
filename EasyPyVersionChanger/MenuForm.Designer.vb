f8fc169a106df47179eae0d7d0b2892e4c750e2f
Jonathan Frederic <xh3xx.goose@gmail.com>
2013-08-07 13:13:31 -0700

    Initial commit

---
+++ EasyPyVersionChanger/MenuForm.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AddButton = New System.Windows.Forms.Button()
+        Me.SystemPathCheckList = New System.Windows.Forms.CheckedListBox()
+        Me.PythonCheckBox = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'AddButton
+        '
+        Me.AddButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.AddButton.Location = New System.Drawing.Point(385, 230)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Size = New System.Drawing.Size(75, 23)
+        Me.AddButton.TabIndex = 0
+        Me.AddButton.Text = "Add"
+        Me.AddButton.UseVisualStyleBackColor = True
+        '
+        'SystemPathCheckList
+        '
+        Me.SystemPathCheckList.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SystemPathCheckList.CheckOnClick = True
+        Me.SystemPathCheckList.FormattingEnabled = True
+        Me.SystemPathCheckList.Location = New System.Drawing.Point(12, 12)
+        Me.SystemPathCheckList.Name = "SystemPathCheckList"
+        Me.SystemPathCheckList.Size = New System.Drawing.Size(448, 199)
+        Me.SystemPathCheckList.TabIndex = 1
+        '
+        'PythonCheckBox
+        '
+        Me.PythonCheckBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.PythonCheckBox.AutoSize = True
+        Me.PythonCheckBox.Checked = True
+        Me.PythonCheckBox.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.PythonCheckBox.Location = New System.Drawing.Point(12, 236)
+        Me.PythonCheckBox.Name = "PythonCheckBox"
+        Me.PythonCheckBox.Size = New System.Drawing.Size(83, 17)
+        Me.PythonCheckBox.TabIndex = 2
+        Me.PythonCheckBox.Text = "Python Only"
+        Me.PythonCheckBox.UseVisualStyleBackColor = True
+        '
+        'MenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 265)
+        Me.Controls.Add(Me.PythonCheckBox)
+        Me.Controls.Add(Me.SystemPathCheckList)
+        Me.Controls.Add(Me.AddButton)
+        Me.Name = "MenuForm"
+        Me.ShowIcon = False
+        Me.Text = "Easy Python Version Switcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AddButton As System.Windows.Forms.Button
+    Friend WithEvents SystemPathCheckList As System.Windows.Forms.CheckedListBox
+    Friend WithEvents PythonCheckBox As System.Windows.Forms.CheckBox
+
+End Class
---
+++ EasyPyVersionChanger/MenuForm.vb
@@ -0,0 +1,121 @@
+﻿''' <summary>
+''' Main form that contains all of the application's code.
+''' </summary>
+Public Class MenuForm
+
+#Region "Declarations"
+
+    Private mLoaded As Boolean = False
+    Private mApplyCheck As Boolean = False
+
+#End Region
+
+#Region "Events"
+
+    ''' <summary>
+    ''' Form load event
+    ''' </summary>
+    Private Sub OnFormLoad(sender As Object, e As EventArgs) Handles Me.Load
+        Me.LoadState()
+    End Sub
+
+    ''' <summary>
+    ''' Event that fires when the user wants to add a path.
+    ''' </summary>
+    Private Sub OnAddPath(sender As Object, e As EventArgs) Handles AddButton.Click
+        Dim FBD As New FolderBrowserDialog()
+        If Not FBD.ShowDialog = Windows.Forms.DialogResult.Cancel Then
+            Dim Directory As String = FBD.SelectedPath
+            If IO.Directory.Exists(Directory) Then
+                Me.SystemPathCheckList.Items.Add(Directory, True)
+                Me.SaveState()
+            End If
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Event that fires when the state is changed by the user.
+    ''' </summary>
+    Private Sub OnStateChanged(sender As Object, e As ItemCheckEventArgs) Handles SystemPathCheckList.ItemCheck
+        If Not mApplyCheck Then
+            mApplyCheck = True
+
+            Me.SystemPathCheckList.SetItemChecked(e.Index, e.NewValue)
+            Me.SaveState()
+
+            mApplyCheck = False
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Event that fires when the python filter is enabled or disabled.
+    ''' </summary>
+    Private Sub OnPythonCheckChanged(sender As Object, e As EventArgs) Handles PythonCheckBox.CheckedChanged
+        Me.LoadState()
+    End Sub
+
+#End Region
+
+#Region "Support Methods"
+
+    ''' <summary>
+    ''' Load the current state from the system.
+    ''' </summary>
+    Private Sub LoadState()
+        mLoaded = False
+        Me.SystemPathCheckList.Items.Clear()
+
+        'Apply filter
+        Dim Filter As String = String.Empty
+        If Me.PythonCheckBox.Checked Then
+            Filter = "python"
+        End If
+
+        'Read enabled items
+        Dim Path As String = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine)
+        Dim ExistingDirectories As New List(Of String)
+        For Each Directory As String In Path.Split(";")
+            If Not ExistingDirectories.Contains(Directory.ToLower()) AndAlso Directory.ToLower().Contains(Filter.ToLower()) Then
+                Me.SystemPathCheckList.Items.Add(Directory, True)
+                ExistingDirectories.Add(Directory.ToLower())
+            End If
+        Next Directory
+
+        'Read the disabled items if they exist.
+        If IO.File.Exists("disabled.txt") Then
+            Dim DisabledItems() As String = IO.File.ReadAllLines("disabled.txt")
+            For Each Directory As String In DisabledItems
+                If Not ExistingDirectories.Contains(Directory.ToLower()) AndAlso Directory.ToLower().Contains(Filter.ToLower()) Then
+                    Me.SystemPathCheckList.Items.Add(Directory, False)
+                    ExistingDirectories.Add(Directory.ToLower())
+                End If
+            Next Directory
+        End If
+
+        mLoaded = True
+    End Sub
+
+    ''' <summary>
+    ''' Commit the changes to the system.
+    ''' </summary>
+    Private Sub SaveState()
+        If mLoaded Then
+            Dim EnabledDirectories As New List(Of String)
+            Dim DisabledDirectories As New List(Of String)
+            For Index As Integer = 0 To Me.SystemPathCheckList.Items.Count - 1
+                If Me.SystemPathCheckList.GetItemChecked(Index) Then
+                    EnabledDirectories.Add(Me.SystemPathCheckList.Items(Index))
+                Else
+                    DisabledDirectories.Add(Me.SystemPathCheckList.Items(Index))
+                End If
+            Next Index
+
+            Dim Value As String = String.Join(";", EnabledDirectories.ToArray)
+            Environment.SetEnvironmentVariable("PATH", Value, EnvironmentVariableTarget.Machine)
+            IO.File.WriteAllLines("disabled.txt", DisabledDirectories)
+        End If
+    End Sub
+
+#End Region
+
+End Class
